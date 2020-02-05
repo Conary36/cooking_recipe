fb629ed1a71fda2ef9075b314573045cfbf78cde
Conary36 <cbeckford144@gmail.com>
2020-02-05 11:49:27 -0500

    created ingredients page

--- src/App.js
+++ src/App.js
@@ -15,14 +15,39 @@ const sampleRecipes = [
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
-    instructions: "1. Put salt on Chicken\n2. Put Chicken in Oven\n3. Eat Chicken"
+    instructions: "1. Put salt on Chicken\n2. Put Chicken in Oven\n3. Eat Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '2 Tablespoon'
+      }
+    ]
+
   },
   {
     id: 2,
     name: 'Pork',
     servings: 5,
     cookTime: '1:30',
-    instructions: "1. Put salt on Pork\n2. Put Pork in Oven\n3. Eat Pork"
+    instructions: "1. Put salt on Pork\n2. Put Pork in Oven\n3. Eat Pork",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '1 Tablespoon'
+      }
+    ]
   }
 ]
 
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import IngredientList from './IngredientList'
 
 export default function Recipe(props) {//enter props as parameter
     const { //Deconstruct in this structure when there are many variables set equal to props
          name, 
          cookTime, 
          servings, 
-         instructions 
+         instructions,
+         ingredients
     } = props
-    
+
     return (
         <div>
             <div>
@@ -29,6 +31,12 @@ export default function Recipe(props) {//enter props as parameter
                 <span>Instructions</span>
                 <div>
                     {instructions}
+                </div>
+            </div>
+            <div>
+                <span>Ingredients</span>
+                <div>
+                    <IngredientList ingredients={ingredients}/>
                 </div>
             </div>
         </div>
